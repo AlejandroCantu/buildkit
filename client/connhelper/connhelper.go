0d7485858d475a3c4bf29707ecc287d32a700de1
AlejandroCantu <alejandro.cantu1018@gmail.com>
2021-12-09 19:49:03 -0600

    Fix for issue #2032, "connhelper: support ssh://user@host"

--- client/connhelper/connhelper.go
+++ client/connhelper/connhelper.go
@@ -6,6 +6,10 @@ import (
 	"context"
 	"net"
 	"net/url"
+
+	"github.com/docker/cli/cli/connhelper/commandconn"
+	"github.com/docker/cli/cli/connhelper/ssh"
+	"github.com/pkg/errors"
 )
 
 var helpers = map[string]func(*url.URL) (*ConnectionHelper, error){}
@@ -16,12 +20,39 @@ type ConnectionHelper struct {
 	ContextDialer func(ctx context.Context, addr string) (net.Conn, error)
 }
 
+// GetConnectionHelperWithSSHOpts returns Docker-specific connection helper for
+// the given URL, and accepts additional options for ssh connections. It returns
+// nil without error when no helper is registered for the scheme.
+//
+// Requires Docker 18.09 or later on the remote host.
+func GetConnectionHelperWithSSHOpts(daemonURL string, sshFlags []string) (*ConnectionHelper, error) {
+	return getConnectionHelper(daemonURL, sshFlags)
+}
+
 // GetConnectionHelper returns BuildKit-specific connection helper for the given URL.
 // GetConnectionHelper returns nil without error when no helper is registered for the scheme.
 func GetConnectionHelper(daemonURL string) (*ConnectionHelper, error) {
+	return getConnectionHelper(daemonURL, nil)
+}
+
+func getConnectionHelper(daemonURL string, sshFlags []string) (*ConnectionHelper, error) {
 	u, err := url.Parse(daemonURL)
 	if err != nil {
 		return nil, err
+	}
+
+	// Written for https://github.com/moby/buildkit/issues/2032
+	if u.Scheme == "ssh" {
+		sp, err := ssh.SpecFromURL(daemonURL)
+		if err != nil {
+			return nil, errors.Wrap(err, "ssh host connection is not valid")
+		}
+		return &ConnectionHelper{
+			Dialer: func(ctx context.Context, network, addr string) (net.Conn, error) {
+				return commandconn.New(ctx, "ssh", append(sshFlags, sp.Args("docker", "system", "dial-stdio")...)...)
+			},
+			Host: "http://docker.example.com",
+		}, nil
 	}
 
 	fn, ok := helpers[u.Scheme]
@@ -31,6 +62,8 @@ func GetConnectionHelper(daemonURL string) (*ConnectionHelper, error) {
 
 	return fn(u)
 }
+
+
 
 // Register registers new connectionhelper for scheme
 func Register(scheme string, fn func(*url.URL) (*ConnectionHelper, error)) {
